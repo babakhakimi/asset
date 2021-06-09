84deae2d148ef9064f099f172b588f1f8048a5c4
babakhakimimehr <babakhakimimehr@gmail.com>
2021-06-10 02:45:56 +0430

    Edit Asset Facade

--- src/Facades/Asset.php
+++ src/Facades/Asset.php
@@ -4,17 +4,17 @@
 namespace Lorito\Support\Facades;
 
 /**
- * @method \Lorito\Asset\Asset container(string $container = 'default')
- * @method \Lorito\Asset\Asset addVersioning()
- * @method \Lorito\Asset\Asset removeVersioning()
- * @method \Lorito\Asset\Asset prefix(?string $path = null)
- * @method \Lorito\Asset\Asset add(string|array $name, string $source, string|array $dependencies = [], string|array $attributes = [], string|array $replaces = [])
- * @method \Lorito\Asset\Asset style(string|array $name, string $source, string|array $dependencies = [], string|array $attributes = [], string|array $replaces = [])
- * @method \Lorito\Asset\Asset script(string|array $name, string $source, string|array $dependencies = [], string|array $attributes = [], string|array $replaces = [])
- * @method string styles()
- * @method string scripts()
- * @method string show()
- * @method string toHtml()
+ * @method static \Lorito\Asset\Asset container(string $container = 'default')
+ * @method static \Lorito\Asset\Asset addVersioning()
+ * @method static \Lorito\Asset\Asset removeVersioning()
+ * @method static \Lorito\Asset\Asset prefix(?string $path = null)
+ * @method static \Lorito\Asset\Asset add(string|array $name, string $source, string|array $dependencies = [], string|array $attributes = [], string|array $replaces = [])
+ * @method static \Lorito\Asset\Asset style(string|array $name, string $source, string|array $dependencies = [], string|array $attributes = [], string|array $replaces = [])
+ * @method static \Lorito\Asset\Asset script(string|array $name, string $source, string|array $dependencies = [], string|array $attributes = [], string|array $replaces = [])
+ * @method static string styles()
+ * @method static string scripts()
+ * @method static string show()
+ * @method static string toHtml()
  *
  * @see \Lorito\Asset\Factory
  */
